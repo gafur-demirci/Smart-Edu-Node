105528428f6a100684a78aed959172282e670fe2
gafur-demirci <gafur.gs.68gmail.com>
2021-11-29 17:58:46 +0300

    Page routes added

---
+++ routes/pageRoute.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const pageController = require('../controllers/pageController');
+
+const router = express.Router();
+
+// routes
+
+router.route('/').get(pageController.getIndexPage);
+router.route('/about').get(pageController.getAboutPage);
+router.route('/courses').get(pageController.getCoursesPage);
+router.route('/dashboard').get(pageController.getDashboardPage);
+router.route('/contact').get(pageController.getContactPage);
+
+
+module.exports = router;
