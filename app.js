b9557cad5be552091df9d51cbdfe0c0a040829b8
gafur-demirci <gafur.gs.68gmail.com>
2021-11-29 22:14:55 +0300

    Updated

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 
 const app = express();
+
+// Connect Database
+mongoose.connect('mongodb://localhost/smart-edu-db').then(() => { console.log('db connected!')});
 
 // Template Engine
 app.set('view engine', 'ejs');
 
 // Middlewares
 app.use(express.static('public'));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) 
 
 // Routes
 
 app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+
 /* app.get('/about', pageComtroller.getAboutPage);
 app.get('/courses', pageComtroller.getCoursesPage);
 app.get('/dashboard', pageComtroller.getDashboardPage);
