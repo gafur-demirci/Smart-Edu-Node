908404128e736bc94aef8a0d4d217fa8c4e0ca78
gafur-demirci <gafur.gs.68gmail.com>
2021-11-29 17:13:29 +0300

    Routing

--- app.js
+++ app.js
@@ -2,6 +2,44 @@ const express = require('express');
 
 const app = express();
 
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'));
+
+// Routes
+
+app.get('/', (req, res) => {
+    res.status(200).render('index', {
+        page_name : "index"
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name : "about"
+    });
+});
+
+app.get('/courses', (req, res) => {
+    res.status(200).render('courses', {
+        page_name : "courses"
+    });
+});
+
+app.get('/dashboard', (req, res) => {
+    res.status(200).render('dashboard', {
+        page_name : "dashboard"
+    });
+});
+
+app.get('/contact', (req, res) => {
+    res.status(200).render('contact', {
+        page_name : "contact"
+    });
+});
+
 const port = 3000;
 
 app.listen(port, () => {
