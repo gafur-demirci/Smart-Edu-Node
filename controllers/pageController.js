7363c47e2319c747ad768fccc8da7b415b9448b7
gafur-demirci <gafur.gs.68gmail.com>
2021-11-29 17:58:09 +0300

    Page controllers added

---
+++ controllers/pageController.js
@@ -0,0 +1,29 @@
+exports.getIndexPage = (req, res) => {
+    res.status(200).render('index', {
+        page_name: 'index',
+    });
+};
+
+exports.getAboutPage = (req, res) => {
+    res.status(200).render('about', {
+        page_name: 'about',
+    });
+};
+
+exports.getCoursesPage = (req, res) => {
+    res.status(200).render('courses', {
+        page_name: 'courses',
+    });
+};
+
+exports.getDashboardPage = (req, res) => {
+    res.status(200).render('dashboard', {
+        page_name: 'dashboard',
+    });
+};
+
+exports.getContactPage = (req, res) => {
+    res.status(200).render('contact', {
+        page_name: 'contact',
+    });
+};
