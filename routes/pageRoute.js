79ed93a146ad8f9a7fcbb476f547fbfea2d200a0
gafur-demirci <gafur.gs.68gmail.com>
2021-12-01 21:59:45 +0300

    custom mv added

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const pageController = require('../controllers/pageController');
+const redirectMiddleware = require('../middlewares/redirectMiddleware');
 
 const router = express.Router();
 
@@ -8,7 +9,9 @@ const router = express.Router();
 router.route('/').get(pageController.getIndexPage);
 router.route('/about').get(pageController.getAboutPage);
 router.route('/contact').get(pageController.getContactPage);
-router.route('/login').get(pageController.getLoginPage);
-router.route('/register').get(pageController.getRegisterPage);
+router.route('/login').get(redirectMiddleware ,pageController.getLoginPage);
+router.route('/register').get(redirectMiddleware, pageController.getRegisterPage);
+// /login veya /register a manuel olarak erişilmek istendiğinde önce ilk sırdaki middleware çalışacak
+// bu sayede login durumda kullanıcı varsa index e yönlendirilmesini sağlıyoruz.
 
 module.exports = router;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
 
 const router = express.Router();
 
 // Routes
 
-router.route('/signup').post(authController.createUser); // localhost:3000/users/signup
-router.route('/login').post(authController.loginUser);   // locolhost:3000/users/login
-router.route('/logout').get(authController.logoutUser);  // localhost:3000/users/logout
-router.route('/dashboard').get(authController.getDashboardPage); 
+router.route('/signup').post(authController.createUser);                            // localhost:3000/users/signup                              // 
+router.route('/logout').get(authController.logoutUser);                             // localhost:3000/users/logout
+router.route('/dashboard').get(authMiddleware, authController.getDashboardPage);    // localhost:3000/users/dashboard 
+// users/dashboard a manuel olarak erişilmek istendiğinde önce ilk sırdaki middleware çalışacak
+// bu sayede logout durumda kullanıcı olmadığı için logine yönlendirilmesin sağlıyoruz.
 
 module.exports = router;
