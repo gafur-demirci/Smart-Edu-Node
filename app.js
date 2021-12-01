a1dba5ca6910794f58e6b6238de04c3e680c22ae
gafur-demirci <gafur.gs.68gmail.com>
2021-12-01 20:07:18 +0300

    session routes added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -15,13 +16,27 @@ mongoose.connect('mongodb://localhost/smart-edu-db').then(() => {
 // Template Engine
 app.set('view engine', 'ejs');
 
+// Global Variables
+
+global.userIN = null;
+
 // Middlewares
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+    session({
+        secret: 'keyboard_cat',
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 
 // Routes
-
+app.use('*', (req, res, next) => {
+    userIN = req.session.userID;
+    next();
+});
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/categories', categoryRoute);
