d844cb020cfe258cbe7a20ba6b400744d36410b3
gafur-demirci <gafur.gs.68gmail.com>
2021-12-01 20:57:53 +0300

    added getDashboardPage func

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,7 +26,7 @@ exports.loginUser = (req, res) => {
                     if (same) {
                         // USER SESSION
                         req.session.userID = user._id;
-                        res.status(200).redirect('/');
+                        res.status(200).redirect('/users/dashboard');
                     }
                 });
             }
@@ -42,5 +42,13 @@ exports.loginUser = (req, res) => {
 exports.logoutUser = (req, res) => {
     req.session.destroy(() => {
         res.redirect('/');
+    });
+};
+
+exports.getDashboardPage = async (req, res) => {
+    const user = await User.findOne({ _id : req.session.userID });
+    res.status(200).render('dashboard', {
+        user,
+        page_name: 'dashboard',
     });
 };
