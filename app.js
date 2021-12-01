d4f9a374e49dd087e422c446e895588a9953dafb
gafur-demirci <gafur.gs.68gmail.com>
2021-12-01 16:34:46 +0300

    Category added

--- app.js
+++ app.js
@@ -2,25 +2,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
-
+const categoryRoute = require('./routes/categoryRoute');
 
 const app = express();
 
 // Connect Database
-mongoose.connect('mongodb://localhost/smart-edu-db').then(() => { console.log('db connected!')});
+mongoose.connect('mongodb://localhost/smart-edu-db').then(() => {
+    console.log('db connected!');
+});
 
 // Template Engine
 app.set('view engine', 'ejs');
 
 // Middlewares
 app.use(express.static('public'));
-app.use(express.json())
-app.use(express.urlencoded({ extended: true })) 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
 
 /* app.get('/about', pageComtroller.getAboutPage);
 app.get('/courses', pageComtroller.getCoursesPage);
