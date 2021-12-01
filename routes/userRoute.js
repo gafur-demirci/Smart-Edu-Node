f323630630355ba92c3b981d0ccb0456ebdf20c4
gafur-demirci <gafur.gs.68gmail.com>
2021-12-01 19:20:04 +0300

    login route added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 // Routes
 
-router.route('/signup').post(authController.createUser); // localhost:/users/signup
+router.route('/signup').post(authController.createUser); // localhost:3000/users/signup
+router.route('/login').post(authController.loginUser);   // locolhost:3000/users/login
 
 module.exports = router;
