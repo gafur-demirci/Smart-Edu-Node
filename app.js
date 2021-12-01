a6c7a6267163fbca5e1719606d985078518f7888
gafur-demirci <gafur.gs.68gmail.com>
2021-12-01 20:29:55 +0300

    MongoStore added for restart then not log out

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -29,6 +30,9 @@ app.use(
         secret: 'keyboard_cat',
         resave: false,
         saveUninitialized: true,
+        store: MongoStore.create({
+            mongoUrl: 'mongodb://localhost/smart-edu-db'
+        }),
     })
 );
 
