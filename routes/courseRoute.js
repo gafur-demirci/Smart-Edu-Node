cf6997efd8f7c465fbd131a77e32c8f085749998
gafur-demirci <gafur.gs.68gmail.com>
2021-12-03 00:21:12 +0300

    added enroll route

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const courseController = require('../controllers/courseController');
 const roleMiddleware = require('../middlewares/roleMiddleware');
+const Course = require('../models/Course');
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ const router = express.Router();
 router.route('/').post(roleMiddleware(['teacher', 'admin']), courseController.createCourse); // localhost:/courses/  (kurs ekleme)
 router.route('/').get(courseController.getAllCourses); // localhost:/courses/  (kursları listeleme)
 router.route('/:slug').get(courseController.getCourse); // localhost:/courses/id  (tekil kurs getirme)
+router.route('/enroll').post(courseController.enrollCourse); // localhost:3000/courses/enroll
 
 module.exports = router;
