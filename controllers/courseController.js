fe2377ecb94744da343e16cb801f123f876fe24b
gafur-demirci <gafur.gs.68gmail.com>
2021-12-03 20:50:57 +0300

    course update added

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -137,3 +137,25 @@ exports.deleteCourse = async (req, res) => {
         });
     }
 };
+
+exports.updateCourse = async (req, res) => {
+    try {
+
+        const course = await Course.findOne({ slug: req.params.slug });
+        course.name = req.body.name;
+        course.description = req.body.description;
+        course.category = req.body.category;
+
+        course.save();
+
+        // req.flash('success', `${course.name} has been updated successfully`);
+
+        res.status(200).redirect('/users/dashboard');
+
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error,
+        });
+    }
+};
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -10,7 +10,8 @@ const router = express.Router();
 router.route('/').post(roleMiddleware(['teacher', 'admin']), courseController.createCourse); // localhost:/courses/  (kurs ekleme)
 router.route('/').get(courseController.getAllCourses);                                       // localhost:/courses/  (kursları listeleme)
 router.route('/:slug').get(courseController.getCourse);                                      // localhost:/courses/id  (tekil kurs getirme)
-router.route('/:slug').delete(courseController.deleteCourse);                                   // localhost:/courses/slug?_method=DELETE (kurs silme)
+router.route('/:slug').delete(courseController.deleteCourse);                                // localhost:/courses/slug?_method=DELETE (kurs silme)
+router.route('/:slug').put(courseController.updateCourse);                                   // localhost:/courses/slug?_method=DELETE (kurs güncelleme)
 router.route('/enroll').post(courseController.enrollCourse);                                 // localhost:3000/courses/enroll
 router.route('/release').post(courseController.releaseCourse);                               // localhost:3000/courses/release
 
