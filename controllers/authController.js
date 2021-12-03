06b0dad106d7735522abc06dde078f20530c3a99
gafur-demirci <gafur.gs.68gmail.com>
2021-12-03 18:01:51 +0300

    express-validator added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 const User = require('../models/User');
 const Course = require('../models/Course');
 const Category = require('../models/Category');
@@ -9,10 +10,12 @@ exports.createUser = async (req, res) => {
         
         res.status(201).redirect('/login');
     } catch (error) {
-        res.status(400).json({
-            status: 'fail',
-            error,
-        });
+        const errors = validationResult(req);
+        for (let i=0; i<errors.array().length; i++ ) {
+
+            req.flash('error', `${errors.array()[i].msg}`);
+        }
+        res.status(400).redirect('/register');
     }
 };
 
@@ -23,12 +26,20 @@ exports.loginUser = (req, res) => {
         User.findOne({ email }, (err, user) => {
             if (user) {
                 bcrypt.compare(password, user.password, (err, same) => {
-
+                    if (same) {
                         // USER SESSION
                         req.session.userID = user._id;
                         res.status(200).redirect('/users/dashboard');
+                    } else {
+                        req.flash('error', 'Your Password is not correct!');
+                        res.status(400).redirect('/login');
+                    }
+                        
                         
                 });
+            } else {
+                req.flash('error', 'User is not exist!');
+                res.status(400).redirect('/login');
             }
         });
     } catch (error) {
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const { body } = require('express-validator');
+const User = require('../models/User');
 
 
 const router = express.Router();
 
 // Routes
 
-router.route('/signup').post(authController.createUser);                            // localhost:3000/users/signup                              // 
+router.route('/signup').post(
+    [
+        body('name').not().isEmpty().withMessage('Please enter your name'),
+
+
+        body('email').isEmail().withMessage('Please enter valid Email')
+        .custom((userEmail) => {
+            return User.findOne({email : userEmail}).then(user => {
+                if (user) {
+                    return Promise.reject('This Email is already exist!');
+                }
+            })
+        }),
+
+
+        body('password').not().isEmpty().withMessage('Please enter A password'),
+
+    ],authController.createUser);                                                   // localhost:3000/users/signup 
 router.route('/login').post(authController.loginUser);                              // localhost:3000/users/login
 router.route('/logout').get(authController.logoutUser);                             // localhost:3000/users/logout
 router.route('/dashboard').get(authMiddleware, authController.getDashboardPage);    // localhost:3000/users/dashboard 
