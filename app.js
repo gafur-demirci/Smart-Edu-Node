33017f285f2b6feeb4595e3464ef8d0053ae74e1
gafur-demirci <gafur.gs.68gmail.com>
2021-12-03 18:54:45 +0300

    method-override added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
+const methodOverride = require('method-override');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -32,15 +33,20 @@ app.use(
         resave: false,
         saveUninitialized: true,
         store: MongoStore.create({
-            mongoUrl: 'mongodb://localhost/smart-edu-db'
+            mongoUrl: 'mongodb://localhost/smart-edu-db',
         }),
     })
 );
 app.use(flash());
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     res.locals.flashMessages = req.flash();
     next();
 });
+app.use(
+    methodOverride('_method', {
+        methods: ['POST', 'GET'],
+    })
+);
 
 // Routes
 app.use('*', (req, res, next) => {
